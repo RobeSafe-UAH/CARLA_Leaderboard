185ccd3caa7f1cd75ff0763dc9d9a4da06997a4d
Cram3r95 <cram3r95@gmail.com>
2021-10-05 18:34:06 +0200

    Stable version of Dockerfile, make_docker.sh and code synchronization host-container

--- build_image/setup_carla.sh
+++ build_image/setup_carla.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+cd /workspace
 git clone -b stable --single-branch https://github.com/carla-simulator/leaderboard.git
 git clone -b leaderboard --single-branch https://github.com/carla-simulator/scenario_runner.git
-cd /workspace
