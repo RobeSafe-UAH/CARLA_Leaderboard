4e9f0bd4c3684aea355a17ccdeb3211294a9d739
Cram3r95 <cram3r95@gmail.com>
2021-10-06 00:12:37 +0200

    Fix make_docker.sh error, .tmp folder was stored again

--- build_image/make_docker.sh
+++ build_image/make_docker.sh
@@ -2,7 +2,7 @@
 
 rm -rf .tmp
 mkdir -p .tmp
-cd .. && rsync -Rr . ./build_image/.tmp
+cd .. && rsync --exclude build_image/.tmp -Rr . ./build_image/.tmp
 cd ./build_image/
 
 # set --force-rm if you want to build the image from scratch, removing intermediate container
