3cc35ecd3b3c9b1245aa4a84267febb62f63d3ca
Cram3r95 <cram3r95@gmail.com>
2021-10-21 12:42:07 +0000

    Add add_models.sh to download the neural network models from server

--- build_image/add_models.sh
+++ build_image/add_models.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 address="www.robesafe.uah.es"
+yolov5_folder=".tmp/catkin_ws/src/t4ac_unified_perception_layer/models/yolov5"
+estimation_3d_folder=".tmp/catkin_ws/src/t4ac_unified_perception_layer/models/3d_estimation"
 
 # Perception
 
-scp $1@$address:/home/git/techs4agecar/models/yolov5/yolov5l.pt .tmp
-scp $1@$address:/home/git/techs4agecar/models/3d_estimation/epoch_10.pkl .tmp
-scp $1@$address:/home/git/techs4agecar/models/3d_estimation/vgg19_bn-c79401a0.pth .tmp
+if [ -d $yolov5_folder ] 
+then 
+    continue
+else 
+    mkdir $yolov5_folder
+fi
+scp $1@$address:/home/git/techs4agecar/models/yolov5/yolov5l.pt $yolov5_folder/yolov5l.pt
+
+if [ -d $estimation_3d_folder ] 
+then 
+    continue
+else 
+    mkdir $estimation_3d_folder
+fi
+
+scp $1@$address:/home/git/techs4agecar/models/3d_estimation/epoch_10.pkl $estimation_3d_folder/epoch_10.pkl
+scp $1@$address:/home/git/techs4agecar/models/3d_estimation/vgg19_bn-c79401a0.pth $estimation_3d_folder/vgg19_bn-c79401a0.pth
--- build_image/make_docker.sh
+++ build_image/make_docker.sh
@@ -3,16 +3,14 @@
 rm -rf .tmp
 mkdir -p .tmp
 cd .. && rsync --exclude build_image/.tmp -Rr . ./build_image/.tmp
+cd build_image
 
 # Add models
 
-add_models.sh $2
-
-#
-
-cd ./build_image/
+current_folder=$PWD
+source $current_folder/add_models.sh $2
 
 # set --force-rm if you want to build the image from scratch, removing intermediate container
-#docker build -t $1 . 
+docker build -t $1 . 
 
-#rm -rf .tmp
+rm -rf .tmp
