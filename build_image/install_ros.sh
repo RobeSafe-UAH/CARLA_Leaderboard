fdbfe897b7ac19a6a2904b5f2f0772d5d15bf1ac
Cram3r95 <cram3r95@gmail.com>
2022-02-09 12:35:17 +0000

    remove visualization files. Update Dockerfile

--- build_image/install_ros.sh
+++ build_image/install_ros.sh
@@ -3,9 +3,12 @@
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt update -y
-DEBIAN_FRONTEND="noninteractive" apt install  -y ros-noetic-desktop-full
+DEBIAN_FRONTEND="noninteractive" apt install -y ros-noetic-desktop-full
+DEBIAN_FRONTEND="noninteractive" apt install -y ros-noetic-pcl-ros
 DEBIAN_FRONTEND="noninteractive" apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool \
                                                 ros-noetic-geographic-msgs ros-noetic-geodesy ros-noetic-vision-msgs libgeographic-dev \
-                                                ros-noetic-ros-numpy 
+                                                ros-noetic-ros-numpy ros-noetic-visualization-msgs ros-noetic-diagnostic-updater \
+                                                ros-noetic-eigen-conversions ros-noetic-tf2-geometry-msgs ros-noetic-roslint
+
 rosdep init
 rosdep update
